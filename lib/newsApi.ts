134ca66d87c41d81a1a865961844952a039aad31
Kalki2898AD <rishikeshvarma9854@gmail.com>
2025-02-14 18:34:21 +0530

    newsapi issues

--- lib/newsApi.ts
+++ lib/newsApi.ts
@@ -4,14 +4,15 @@ import { Article } from '../types';
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 const NEWS_API_BASE_URL = 'https://newsapi.org/v2';
 
-export async function fetchNews() {
+export async function fetchNews(query?: string): Promise<Article[]> {
   try {
     const response = await axios.get(`${NEWS_API_BASE_URL}/top-headlines`, {
       params: {
         apiKey: NEWS_API_KEY,
         category: 'technology',
         language: 'en',
-        pageSize: 10
+        pageSize: 10,
+        q: query
       }
     });
     return response.data.articles;
